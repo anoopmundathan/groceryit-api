1473e794fdb701d8bcfb7535965983ea86ef9b96
Anoop Mundathan <anoop.mundathan@gmail.com>
2018-01-02 07:36:57 +0000

    Add morgan for logging

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const logger = require('morgan');
 const routes = require('./routes');
-const app = express();
 
+const app = express();
 app.use(logger('dev'));
+
 app.use('/api/v1/stores', routes);
 
 const PORT = process.env.PORT || 3000;
