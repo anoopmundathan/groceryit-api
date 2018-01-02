4cd431378c6c211bbad141332027635a4363c19c
Anoop Mundathan <anoop.mundathan@gmail.com>
2018-01-02 07:46:17 +0000

    Add body-parser

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const logger = require('morgan');
 const routes = require('./routes');
 
 const app = express();
+
+// Logger
 app.use(logger('dev'));
+
+// Parse application/x-www-form-urlencoded 
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Parse application/json 
+app.use(bodyParser.json());
 
 app.use('/api/v1/stores', routes);
 
