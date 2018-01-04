5214539ff5114b6cc82aaed1ee1e67cbb7cde24c
Anoop Mundathan <anoop.mundathan@gmail.com>
2018-01-04 05:14:48 +0000

    Use Promise for Store

--- controllers/stores.js
+++ controllers/stores.js
@@ -2,20 +2,22 @@ const Store = require('../models/store');
 
 module.exports = {
   getAllStore: (req, res, next) => {
-    Store.find({}, (err, stores) => {
-      if(err) {
-        next(err)
-      }
-      res.status(200).json(stores);
-    })
+    Store.find({})
+      .then(stores => {
+        res.status(200).json(stores)
+      })
+      .catch(err => {
+        next(err);
+      });
   },
   createNewStore: (req, res, next) => {
     const newStore = new Store(req.body);
-    newStore.save((err, store) => {
-      if(err) {
-        next(err)
-      }
-      res.status(201).json(store);
-    })
+    newStore.save()
+      .then(store => {
+        res.status(201).json(store)
+      })
+      .catch(err => {
+        next(err);
+      });
   }
 }
