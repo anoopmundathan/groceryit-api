e8c702dab5227eb49d7dbefab305ad0770de3306
Anoop Mundathan <anoop.mundathan@gmail.com>
2018-01-04 05:29:23 +0000

    Use Async/Await for store

--- controllers/stores.js
+++ controllers/stores.js
@@ -1,23 +1,21 @@
 const Store = require('../models/store');
 
 module.exports = {
-  getAllStore: (req, res, next) => {
-    Store.find({})
-      .then(stores => {
-        res.status(200).json(stores)
-      })
-      .catch(err => {
-        next(err);
-      });
+  getAllStore: async (req, res, next) => {
+    try {
+      const stores = await Store.find({});
+      res.status(200).json(stores);
+    } catch(err) {
+      next(err);
+    }
   },
-  createNewStore: (req, res, next) => {
+  createNewStore: async (req, res, next) => {
     const newStore = new Store(req.body);
-    newStore.save()
-      .then(store => {
-        res.status(201).json(store)
-      })
-      .catch(err => {
-        next(err);
-      });
+    try {
+      const store = await newStore.save();
+      res.status(201).json(store);
+    } catch(err) {
+      next(err);
+    }
   }
 }
